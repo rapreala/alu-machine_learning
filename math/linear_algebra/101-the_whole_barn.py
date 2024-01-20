47f383bbbc3ecb5343b0c1187425c03b18ba5533
Ryan Apreala <r.apreala@alustudent.com>
2024-01-20 17:49:56 +0000

    fix

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
-'''
-    A function def add_matrices(mat1, mat2):
-    that adds two matrices
-'''
+"""Defines `add_matrices`."""
+import numpy as np
 
 
 def add_matrices(mat1, mat2):
-    '''
-        A function def add_matrices(mat1, mat2):
-        that adds two matrices
-    '''
-    if isinstance(mat1, list) and isinstance(mat2, list):
-        if len(mat1) == len(mat2):
-            return [add_matrices(a, b) for a, b in zip(mat1, mat2)]
-    elif isinstance(mat1, (int, float)) and isinstance(mat2, (int, float)):
-        return mat1 + mat2
-    return None
+    """Adds two matrices."""
+    mat1, mat2 = np.array(mat1), np.array(mat2)
+    if not mat1.shape == mat2.shape:
+        return None
+    return mat1 + mat2
