9d18b8e9b2e6047c573ef337ec972480561ab886
Ryan Apreala <r.apreala@alustudent.com>
2024-02-05 22:39:39 +0000

    next

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,46 +1,118 @@
 #!/usr/bin/env python3
-
-
-"""Represents an exponential distribution."""
+""" defines Binomial class that represents binomial distribution """
 
 
 class Binomial:
-    """Represents a binomial distribution."""
+    """
+    class that represents Binomial distribution
+
+    class constructor:
+        def __init__(self, data=None, n=1, p=0.5)
+
+    instance attributes:
+        n [int]: the number of Bernoilli trials
+        p [float]: the probability of a success
+
+    instance methods:
+        def pmf(self, k): calculates PMF for given number of successes
+        def cdf(self, k): calculates CDF for given number of successes
+    """
 
     def __init__(self, data=None, n=1, p=0.5):
-        """Initializes the binomial distribution.
-
-        Args:
-            data (list): A list of data to estimate the distribution.
-            n (int): The number of Bernoulli trials.
-            p (float): The probability of a "success".
-
-        Raises:
-            ValueError: If n is not positive, p is not a valid probability,
-                       or data is invalid.
-            TypeError: If data is not a list.
         """
+        class constructor
 
+        parameters:
+            data [list]: data to be used to estimate the distibution
+            n [int]: the number of Bernoilli trials
+            p [float]: the probability of a success
+
+        Sets the instance attributes n and p
+        If data is not given:
+            Use the given n and p
+            Raise ValueError if n is not positive value
+            Raise ValueError if p is not a valid probability
+        If data is given:
+            Calculate n and p from data, rounding n to nearest int
+            Raise TypeError if data is not a list
+            Raise ValueError if data does not contain at least two data points
+        """
         if data is None:
-            # Use given n and p
-            if n <= 0:
+            if n < 1:
                 raise ValueError("n must be a positive value")
-            if not 0 < p < 1:
+            else:
+                self.n = n
+            if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
-            self.n = int(n)
-            self.p = p
+            else:
+                self.p = p
         else:
-            # Calculate n and p from data
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
+            else:
+                mean = float(sum(data) / len(data))
+                summation = 0
+                for x in data:
+                    summation += ((x - mean) ** 2)
+                variance = (summation / len(data))
+                q = variance / mean
+                p = (1 - q)
+                n = round(mean / p)
+                p = float(mean / n)
+                self.n = n
+                self.p = p
 
-            # Calculate p first
-            self.p = sum(1 for x in data if x == 1) / len(data)
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of successes
 
-            # Calculate n, rounding to nearest integer
-            self.n = round(sum(data) / self.p)
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
 
-            # Recalculate p for consistency
-            self.p = sum(1 for x in data if x == 1) / len(data)
+        return:
+            the PMF value for k
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        p = self.p
+        n = self.n
+        q = (1 - p)
+        n_factorial = 1
+        for i in range(n):
+            n_factorial *= (i + 1)
+        k_factorial = 1
+        for i in range(k):
+            k_factorial *= (i + 1)
+        nk_factorial = 1
+        for i in range(n - k):
+            nk_factorial *= (i + 1)
+        binomial_co = n_factorial / (k_factorial * nk_factorial)
+        pmf = binomial_co * (p ** k) * (q ** (n - k))
+        return pmf
+
+    def cdf(self, k):
+        """
+        calculates the value of the CDF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the CDF value for k
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
