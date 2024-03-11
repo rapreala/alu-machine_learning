4411eb0c9431c4210169c083e6be086d068e1a12
Ryan Apreala <r.apreala@alustudent.com>
2024-03-11 08:43:21 +0000

    final fix

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -40,28 +40,28 @@ class MultiNormal:
         self.cov = cov
 
     def pdf(self, x):
-            """
-            calculates the PDF at a data point
-            Args:
-                x is a numpy.ndarray of shape (d,) containing the data point
-                    whose PDF should be calculated
-                    d is the number of dimensions of the Multinomial instance
-            Returns:
-                the PDF at x
-            """
-            if type(x) is not np.ndarray:
-                raise TypeError("x must be a numpy.ndarray")
-            d = self.cov.shape[0]
-            if len(x.shape) != 2:
-                raise ValueError("x must have the shape ({}, 1)".format(d))
-            test_d, one = x.shape
-            if test_d != d or one != 1:
-                raise ValueError("x must have the shape ({}, 1)".format(d))
+        """
+        calculates the PDF at a data point
+        Args:
+            x is a numpy.ndarray of shape (d,) containing the data point
+                whose PDF should be calculated
+                d is the number of dimensions of the Multinomial instance
+        Returns:
+            the PDF at x
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        test_d, one = x.shape
+        if test_d != d or one != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
 
-            det = np.linalg.det(self.cov)
-            inv = np.linalg.inv(self.cov)
-            pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
-            mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
-            pdf *= np.exp(-0.5 * mult)
-            pdf = pdf[0][0]
-            return pdf
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
+        pdf *= np.exp(-0.5 * mult)
+        pdf = pdf[0][0]
+        return pdf
